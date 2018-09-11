6977c942838adb32ecb934087ad34e477942cc53
Kshitiz19 <skshitij1993@gmail.com>
2018-09-11 12:33:29 +0530

    changed for user input

--- Code/pagerank.cu
+++ Code/pagerank.cu
@@ -1,174 +1,22 @@
-/*
-    This file calls the filereader function to read the a file into a matrix and then transfers data to GPU memory and runs arnoldi and svd in pipeline until convergence.
-    More details about the control flow is described in the powerpoint presentation.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "arnoldi.h"
+#include "pageRankKernels.h"
 #include "matrix.h"
 #include <math.h>
 #include <string.h>
 #include "filereader.h"
-#include <cusolverDn.h>
-#include <cublas_v2.h>
+#include "svd.h"
 #include <cuda_runtime.h>
-#include <pthread.h>
 
-struct ArnoldiArgs
-{
-    double *d_q1;
-    double *d_H1;
-    double *d_Q;
-    double *temp, *d_z;
-    double *d_A, *h_A, *h_q;
-    double *d_v;
-    int k, n;
-};
-
-struct SVDArgs
-{
-    cusolverDnHandle_t cusolverH;
-    int rows;
-    int cols;
-    int lda;
-
-    double *d_H1;
-    double *d_S;
-    double *d_U;
-    double *d_VT;
-
-    int *devInfo;
-    double *d_work;
-    double *d_rwork;
-    int lwork;
-    int n;
-
-    double *d_q1, *d_Q, *d_v;
-};
-
-
-__global__ void check(double* d_norm, int n, double* val)
-{
-    double temp = 0;
-    double f ;
-    for(int i=0; i<n; i++)
-    {
-        f = d_norm[i];
-        temp += f*f;
-    }
-    *val = sqrt(temp);
-}
-
-
-__global__ void calc_norm(double* d_A, double* d_q, int n, double* d_norm)
-{
-    int id = (blockIdx.x*blockDim.x) + threadIdx.x;
-    if(id<n)
-    {
-        double temp = 0;
-        int c = id*n;
-        for(int i=0; i<n; i++, c++) temp += d_A[c]*d_q[i];
-        d_norm[id] = temp - d_q[id];
-    }
-}
-
-
-__global__ void saxpy(double *Q, double* v ,double* q, int n, int k)
-{
-    int id = (blockIdx.x*blockDim.x) + threadIdx.x;
-    if(id < n){
-        double temp = 0;
-        int i = id*(k+1), j = i+k , count=-1;
-
-        for(; i<j; i++) temp += Q[i]*v[++count];
-        q[id] = temp;
-        }
-}
-
-
-void printMatrix(int m, int n, const double*A, int lda, const char* name)
-{
-    for(int row = 0 ; row < m ; row++)
-        {
-            for(int col = 0 ; col < n ; col++)
-                {
-                    double Areg = A[row + col*lda];
-                    printf(" %f ", Areg);
-                }
-            printf("\n");
-        }
-}
-
-
-__global__ void get_v(double* d_VT, double* d_v, int k)
-{
-    int id = threadIdx.x;
-    d_v[id] = d_VT[id*k +k-1+id];
-}
-
-
-__global__ void initCudaMat(double* A, int row, int col)
-{
-    for(int i=0; i<row*col; i++) A[i] = 0;
-}
-
-
-__global__ void printMatt(double* A, int row, int col)
-{
-    int z;
-    for(z=0; z< (row*col); z++)
-    {
-        printf("%f  ", A[z]);
-        if((z+1)%col == 0) printf("\n");
-    }
-}
-
-
-void *threadArnoldi(void *vargp)
-{
-    struct ArnoldiArgs *args = (struct ArnoldiArgs *)vargp;
-    parallelArnoldi(args->d_A, args->d_q1, args->k, args->d_Q, args->d_H1, args->n, args->d_z, args->temp);
-    return NULL;
-}
-
-
-void *threadSVD(void *vargp)
-{
-        struct SVDArgs *args = (struct SVDArgs *)vargp;
-        signed char jobu = 'A';
-        signed char jobvt = 'A';
-        cusolverDnDgesvd (
-        args->cusolverH,
-        jobu,
-        jobvt,
-        args->rows,
-        args->cols,
-        args->d_H1,
-        args->lda,
-        args->d_S,
-        args->d_U,
-        args->lda,
-        args->d_VT,
-        args->lda,
-        args->d_work,
-        args->lwork,
-        args->d_rwork,
-        args->devInfo);
-
-        cudaDeviceSynchronize();
-
-        get_v<<<1, args->cols>>>(args->d_VT, args->d_v, args->cols);
-        saxpy<<<(args->n)/1024 + 1, 1024>>>(args->d_Q, args->d_v, args->d_q1, args->n, args->cols);
-
-    return NULL;
-}
-
+using namespace std;
 
 int main(int argc, char** argv)
 {
+    const int NUM_THREADS = 256;
+    
     cudaError_t err = cudaSuccess;
-    int k = 16;
+    int k = 8;			// experiment with value of k(power of 2 only) for optimality
     int n;
     double tempNorm;
 
@@ -179,14 +27,16 @@ int main(int argc, char** argv)
     double *d_H1;
     double *d_Q;
     double *temp, *d_z;
-    double *d_A, *h_A, *h_q;
+    double* d_A, *h_A, *h_q;
     double *d_v;
-    double *d_H2, *d_q2, *d_Q2;
+
 
     readInput(&A, argv[1], 0.85);
     n = A.A_rows;
+    printf("n = %d\n", n);
 
     matrixTranspose(&A);
+
     double **AMat = A.A;
     h_A = (double*)malloc(sizeof(double)*n*n);
     h_q = (double*)malloc(sizeof(double)*n);
@@ -203,13 +53,17 @@ int main(int argc, char** argv)
     const int rows = k+1;
     const int cols = k;
     const int lda = rows;
-
+/*
+    double U[lda*rows];
+    double VT[lda*cols];
+    double S[cols];
+*/
     double *d_S = NULL;
     double *d_U = NULL;
     double *d_VT = NULL;
+    int *devInfo = NULL;
     double *d_work = NULL;
     double *d_rwork = NULL;
-    int *devInfo = NULL;
     int lwork = 0;
 
     cudaMalloc ((void**)&d_S , sizeof(double)*cols);
@@ -223,112 +77,104 @@ int main(int argc, char** argv)
 
     cudaMalloc((void**) &d_norm, sizeof(double)*n);
     cudaMalloc((void**) &d_A, sizeof(double)*n*n);
-    cudaMalloc((void**) &d_q1, sizeof(double)*n);
-    cudaMalloc((void**) &d_q2, sizeof(double)*n);
     cudaMalloc((void**) &d_H1, sizeof(double)*k*(k+1));
-    cudaMalloc((void**) &d_H2, sizeof(double)*k*(k+1));
+    cudaMalloc((void**) &d_q1, sizeof(double)*n);
     cudaMalloc((void**) &d_Q, sizeof(double)*n*(k+1));
-    cudaMalloc((void**) &d_Q2, sizeof(double)*n*(k+1));
     cudaMalloc((void**) &temp, sizeof(double));
     cudaMalloc((void**) &d_z, sizeof(double)*n);
     cudaMalloc((void**) &d_v, sizeof(double)*k);
+    //cudaMalloc((void**) &d_q2, sizeof(double)*n);
+
 
     cudaMemcpy(d_A, h_A, n*n*sizeof(double), cudaMemcpyHostToDevice);
     cudaMemcpy(d_q1, h_q, n*sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_q2, h_q, n*sizeof(double), cudaMemcpyHostToDevice);
 
     initCudaMat<<<1, 1>>>(d_H1, k+1, k);
     initCudaMat<<<1, 1>>>(d_Q, n, k+1);
     initCudaMat<<<1, 1>>>(d_z, n, 1);
 
-    //thread
-    pthread_t tid1, tid2;
-    //thread
-
-
-    ArnoldiArgs *arnoldiArgs = (struct ArnoldiArgs *)malloc(sizeof(struct ArnoldiArgs));
-    SVDArgs *svdArgs = (struct SVDArgs *)malloc(sizeof(struct SVDArgs));
-
-	arnoldiArgs->d_A = d_A;
-	arnoldiArgs->d_q1 = d_q1;
-	arnoldiArgs->k = k;
-	arnoldiArgs->d_Q = d_Q;
-	arnoldiArgs->d_H1 = d_H1;
-	arnoldiArgs->n = n;
-	arnoldiArgs->d_z = d_z;
-	arnoldiArgs->temp = temp;
-
-
-	svdArgs->cusolverH = cusolverH;
-	svdArgs->rows = rows;
-	svdArgs->cols = cols;
-	svdArgs->lda = lda;
-	svdArgs->d_H1 = d_H2;
-	svdArgs->d_S = d_S;
-	svdArgs->d_U = d_U;
-	svdArgs->d_VT = d_VT;
-	svdArgs->devInfo = devInfo;
-	svdArgs->d_work = d_work;
-	svdArgs->d_rwork = d_rwork;
-	svdArgs->lwork = lwork;
-	svdArgs->n = n;
-	svdArgs->d_q1 = d_q2;
-	svdArgs->d_Q = d_Q2;
-	svdArgs->d_v = d_v;
-
-    for(int fold = 0; fold < 5; fold++)
+    for(int fold = 0; fold < 10; fold++)
     {
         //printf("Fold = %d\n\n", fold);
 
         clock_t begin = clock();
+
         //#############  Calling Arnoldi to get Q and H  ##################
-
-	    pthread_create(&tid1, NULL, threadArnoldi, (void *)arnoldiArgs);
-
-        if(fold > 0) pthread_create(&tid2, NULL, threadSVD, (void *)svdArgs);
+        parallelArnoldi(d_A, d_q1, k, d_Q, d_H1, n, d_z, temp);
 
 
-	    pthread_join(tid1, NULL);
-	    pthread_join(tid2, NULL);
+        //###############  Calling SVD on H-I   #####################
 
-        // update q
+        signed char jobu = 'A'; // all m columns of U
+        signed char jobvt = 'A'; // all n columns of VT
+        cusolverDnDgesvd (
+        cusolverH,
+        jobu,
+        jobvt,
+        rows,
+        cols,
+        (double*)d_H1,
+        lda,
+        (double*)d_S,
+        (double*)d_U,
+        lda,
+        (double*)d_VT,
+        lda,
+        (double*)d_work,
+        lwork,
+        (double*)d_rwork,
+        devInfo);
+        cudaDeviceSynchronize();
 
-    	double *tempH1 = arnoldiArgs->d_H1;
-    	arnoldiArgs->d_H1 = svdArgs->d_H1;
-    	svdArgs->d_H1 = tempH1;
+/*
+        cudaMemcpy(U , d_U , sizeof(double)*lda*rows, cudaMemcpyDeviceToHost);
+        cudaMemcpy(VT, d_VT, sizeof(double)*lda*cols, cudaMemcpyDeviceToHost);
+        cudaMemcpy(S , d_S , sizeof(double)*cols , cudaMemcpyDeviceToHost);
+        cudaMemcpy(&info_gpu, devInfo, sizeof(int), cudaMemcpyDeviceToHost);
 
-    	double *tempq1 = arnoldiArgs->d_q1;
-    	arnoldiArgs->d_q1 = svdArgs->d_q1;
-    	svdArgs->d_q1 = tempq1;
+        printf("S = (matlab base-1)\n");
+        printMatrix(cols, 1, S, lda, "S");
+        printf("=====\n");
 
-    	double *tempQ = arnoldiArgs->d_Q;
-    	arnoldiArgs->d_Q = svdArgs->d_Q;
-    	svdArgs->d_Q = tempQ;
+        printf("U = (matlab base-1)\n");
+        printMatrix(rows, rows, U, lda, "U");
+        printf("=====\n");
 
-        calc_norm<<<n/1024 + 1, 1024>>>(d_A, arnoldiArgs->d_q1, n, d_norm);
+        printf("VT = (matlab base-1)\n");
+        printMatrix(cols, cols, VT, lda, "VT");
+        printf("=====\n");
+*/
+        get_v<<<1, k>>>(d_VT, d_v, k);
+
+        saxpy<<<n/NUM_THREADS + 1, NUM_THREADS, k*sizeof(double)>>>(d_Q, d_v, d_q1, n, k);
+
+        calc_norm<<<n/NUM_THREADS + 1, NUM_THREADS>>>(d_A, d_q1, n, d_norm);
         check<<<1, 1>>>(d_norm, n, temp);
-        cudaMemcpy(&tempNorm, temp, sizeof(double), cudaMemcpyDeviceToHost);
 
-	    printf("Aq - q norm for iteration %d is %.15lf\n", fold, tempNorm);
+        cudaMemcpy(&tempNorm, temp, sizeof(double), cudaMemcpyDeviceToHost);
+        printf("Aq - q norm for iteration %d is %.15lf\n", fold, tempNorm);
 
         clock_t end = clock();
 
         double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
         printf("Time for this iteration = %lf\n", elapsed_secs);
 
-        cudaMemcpy(h_q , d_q1 , sizeof(double)*n, cudaMemcpyDeviceToHost);
-
         if (err != cudaSuccess) printf("Error above: %s\n", cudaGetErrorString(err));
+
+
+        /*if(normDiff < 0.000001){
+            printf("Converged in %d folds.\n", fold);
+            break;
+        }*/
     }
+
+    cudaMemcpy(h_q , d_q1 , sizeof(double)*n, cudaMemcpyDeviceToHost);
 
     cudaFree(d_norm);
     cudaFree(d_A);
     cudaFree(d_H1);
-    cudaFree(d_H2);
     cudaFree(d_Q);
-    cudaFree(d_Q2);
     cudaFree(d_q1);
-    cudaFree(d_q2);
     cudaFree(d_z);
     cudaFree(d_v);
     cudaFree(d_rwork);
@@ -338,18 +184,14 @@ int main(int argc, char** argv)
     cudaFree(d_work);
 
     // end fold
-
-/*
-    int x = 1;
-    if(h_q[0] < 0) x = -1;
-    printf("Eigenvector : \n");
+    //printf("Eigenvector : \n");
     for(int i=0;i < n;i++){
-           printf("%lf\n",x*h_q[i]);
+           //printf("%lf\n",abs(h_q[i]));
         }
-*/
     free(A.A[0]);
     free(A.A);
     free(h_A);
     free(h_q);
+
     return 0;
 }
